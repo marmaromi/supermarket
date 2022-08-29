d4bfc14802bad1c4cb4ab99d3524c24508bb33bd
Ido Marom <marmaromi@gmail.com>
2022-08-29 09:04:17 +0300

    updated product amount interface in cart

--- Frontend/src/app/components/cart-area/cart-item/cart-item.component.ts
+++ Frontend/src/app/components/cart-area/cart-item/cart-item.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnDestroy, OnInit, Renderer2 } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { ProductsInCartModel } from "src/app/models/products-in-cart-model";
+import { CartService } from "src/app/services/cart.service";
+import { NotifyService } from "src/app/services/notify.service";
+import { environment } from "src/environments/environment";
 
 @Component({
     selector: "app-cart-item",
@@ -8,12 +12,52 @@ import { ProductsInCartModel } from "src/app/models/products-in-cart-model";
 })
 export class CartItemComponent implements OnInit {
 
+    public cartForm: FormGroup;
     @Input() cartProduct: ProductsInCartModel;
+    public productImage: string;
+    public cartId: string;
 
 
-    constructor() { }
+
+    constructor(private fb: FormBuilder, private notify: NotifyService, private cartService: CartService, private renderer2: Renderer2) { }
+
+
 
     ngOnInit(): void {
+        if (this.cartProduct.product.imageName) {
+            this.productImage = environment.productsImagesUrl + "/" + this.cartProduct.product.imageName;
+        }
+
+        this.cartForm = this.fb.group({
+            productAmount: this.cartProduct.amount
+        });
+        this.cartId = sessionStorage.getItem("cartId");
     }
+
+    get productAmount() {
+        return this.cartForm.get("productAmount");
+    }
+
+    updatePrice(num: number) {
+        if (num === -1 && this.cartProduct.amount === 1) {
+
+        }
+        else {
+            this.cartProduct.amount = this.cartProduct.amount + num;
+            this.cartProduct.totalProductPrice = this.cartProduct.product.productPrice * this.cartProduct.amount;
+        }
+    }
+
+    onKeydown(event: KeyboardEvent) {        
+        if (event.key === "-" || event.key === "+" || event.key === "e" || event.key === "0") {
+            console.log(event.key);
+            event.preventDefault();
+        }
+    }
+
+    // deleteProductFromCart() {
+    //     this.cartService.deleteProductFromCart(this.cartId, this.cartProduct._id);
+    // }
+
 
 }
